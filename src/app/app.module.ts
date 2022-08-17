b65abd4d8eb7bb0370cbcbb09d0c41f4c2ccb357
FIRST_NAME LAST_NAME <louguefranck.20@gmail.com>
2022-08-17 06:28:11 +0000

    add first component to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HotelListComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/hotel-list/hotel-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-list',
+  templateUrl: './hotel-list.componet.html',
+  styleUrls: [],
+})
+export class HotelListComponent {
+  public title = "Liste d'hotel";
+}
