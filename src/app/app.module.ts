4d422beff1e32d65cdf0088030bad6c7258c7f88
fanetech <louguefranck.20@.com>
2022-08-23 12:03:32 +0000

    learn pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'
 
+registerLocaleData(localeFr, "fr")
 @NgModule({
   declarations: [AppComponent, HotelListComponent],
   imports: [BrowserModule, FormsModule],
