552b3d6e6fcfe6d3dad8e63ac51d1f1aa2fb40b7
fanetech <louguefranck.20@.com>
2022-08-25 10:14:30 +0000

    send param by route and get in commposent

--- src/app/hotel-list/hotel-detail/hotel-detail.component.ts
+++ src/app/hotel-list/hotel-detail/hotel-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IHotel } from '../hotel';
+import { HotelListService } from '../hotel-list.service';
 
 @Component({
   selector: 'app-hotel-detail',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private hotelListService: HotelListService,
+    private router: Router
+  ) { }
+
+  public hotel: any
 
   ngOnInit(): void {
+    let id= this.route.snapshot.paramMap.get('id')
+    if(id){
+     const _id = parseInt(id)
+      this.hotelListService.getHotel().subscribe(
+        (hotels) =>{
+          this.hotel = hotels.find((hotel) => hotel.hotelId === _id)
+          console.log(this.hotel)
+        }
+      )
+
+    }
+    else id = null
   }
 
+  public backToList():void{
+    this.router.navigate(['/hotels'])
+
+  }
 }
